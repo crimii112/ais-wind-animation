2d1c44bf0baf35c571e59ec77e01a81153b5cce1
crimii112 <ycl623@naver.com>
2025-07-17 09:22:18 +0900

    2025.07.17

--- src/components/map/MapNgii.jsx
+++ src/components/map/MapNgii.jsx
@@ -7,7 +7,7 @@ import {
 } from 'ol/interaction';
 import { Tile as TileLayer } from 'ol/layer';
 import { OSM, WMTS } from 'ol/source';
-import { get as getProjection } from 'ol/proj';
+import { get as getProjection, transform } from 'ol/proj';
 import WMTSTileGrid from 'ol/tilegrid/WMTS';
 import { getTopLeft } from 'ol/extent';
 import proj4 from 'proj4';
@@ -32,24 +32,19 @@ const MapNgii = ({ children, id = 'ngii' }) => {
     '+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m +no_defs'
   );
   register(proj4);
-  const epsg5179 = getProjection('EPSG:5179');
-  epsg5179.setExtent([-200000.0, -28024123.62, 31824123.62, 4000000.0]);
+  const epsg = getProjection('EPSG:5179');
+  epsg.setExtent([-200000.0, -28024123.62, 31824123.62, 4000000.0]);
 
   // 측정소 데이터에 맞춘 extent
   const [mapLayer, setMapLayer] = useState([
-    new TileLayer({
-      source: new OSM(),
-      opacity: 0.7,
-      id: 'base',
-    }),
     new TileLayer({
       source: new WMTS({
         url: '/ais/proxy/ngii/hybrid',
         matrixSet: 'EPSG:5179',
         format: 'image/png',
-        projection: epsg5179,
+        projection: epsg,
         tileGrid: new WMTSTileGrid({
-          origin: getTopLeft(epsg5179.getExtent()),
+          origin: getTopLeft(epsg.getExtent()),
           resolutions: [
             2088.96, 1044.48, 522.24, 261.12, 130.56, 65.28, 32.64, 16.32, 8.16,
             4.08, 2.04, 1.02, 0.51, 0.255,
@@ -76,56 +71,51 @@ const MapNgii = ({ children, id = 'ngii' }) => {
         wrapX: true,
         // attributions: [ `<img style="width:96px; height:16px;"src="${urlvalue}/img/process/ms/map/common/img_btoLogo3.png" alt="로고">` ],   // 로고는 나중에 추가해야할 수도 있어서 완전 삭제는 안함
         crossOrigin: 'anonymous',
-        tilePixelRatio: 2,
       }),
       id: 'base',
     }),
-    // new TileLayer({
-    //   // 백지도
-    //   source: new WMTS({
-    //     url: '/ais/proxy/ngii/hybrid',
-    //     matrixSet: 'EPSG:5179',
-    //     format: 'image/png',
-    //     projection: epsg5179,
-    //     tileGrid: new WMTSTileGrid({
-    //       origin: getTopLeft(epsg5179.getExtent()),
-    //       resolutions: [
-    //         2088.96, 1044.48, 522.24, 261.12, 130.56, 65.28, 32.64, 16.32, 8.16,
-    //         4.08, 2.04, 1.02, 0.51, 0.255,
-    //       ],
-    //       matrixIds: [
-    //         'L05',
-    //         'L06',
-    //         'L07',
-    //         'L08',
-    //         'L09',
-    //         'L10',
-    //         'L11',
-    //         'L12',
-    //         'L13',
-    //         'L14',
-    //         'L15',
-    //         'L16',
-    //         'L17',
-    //         'L18',
-    //       ],
-    //     }),
-    //     style: 'korean',
-    //     layer: 'white_map',
-    //     wrapX: true,
-    //     // attributions: [ `<img style="width:96px; height:16px;"src="${urlvalue}/img/process/ms/map/common/img_btoLogo3.png" alt="로고">` ],
-    //     crossOrigin: 'anonymous',
-    //   }),
-    //   id: 'white',
-    //   visible: false,
-    // }),
+    new TileLayer({
+      // 백지도
+      source: new WMTS({
+        url: '/ais/proxy/ngii/hybrid',
+        matrixSet: 'EPSG:5179',
+        format: 'image/png',
+        projection: epsg,
+        tileGrid: new WMTSTileGrid({
+          origin: getTopLeft(epsg.getExtent()),
+          resolutions: [
+            2088.96, 1044.48, 522.24, 261.12, 130.56, 65.28, 32.64, 16.32, 8.16,
+            4.08, 2.04, 1.02, 0.51, 0.255,
+          ],
+          matrixIds: [
+            'L05',
+            'L06',
+            'L07',
+            'L08',
+            'L09',
+            'L10',
+            'L11',
+            'L12',
+            'L13',
+            'L14',
+            'L15',
+            'L16',
+            'L17',
+            'L18',
+          ],
+        }),
+        style: 'korean',
+        layer: 'white_map',
+        wrapX: true,
+        // attributions: [ `<img style="width:96px; height:16px;"src="${urlvalue}/img/process/ms/map/common/img_btoLogo3.png" alt="로고">` ],
+        crossOrigin: 'anonymous',
+      }),
+      id: 'white',
+      visible: false,
+    }),
   ]);
 
   useEffect(() => {
-    const resolutions4326 = Array.from(
-      { length: 18 },
-      (_, z) => 0.703125 / Math.pow(2, z)
-    );
     const map = new OlMap({
       controls: defaultControls({ zoom: true, rotate: false }),
       interactions: defaultInteractions({ mouseWheelZoom: true }).extend([
@@ -133,30 +123,18 @@ const MapNgii = ({ children, id = 'ngii' }) => {
       ]),
       layers: mapLayer,
       view: new View({
-        projection: 'EPSG:4326',
-        center: [127.5, 36.0],
-        // projection: 'EPSG:5179',
-        // center: [960551.04896058, 1819735.5150606],
+        projection: 'EPSG:5179',
+        center: [960551.04896058, 1819735.5150606],
         maxZoom: 18,
         minZoom: 5,
         zoom: 14,
         maxResolution: 2088.96,
         minResolution: 0.255,
         constrainResolution: true,
-        resolutions: resolutions4326,
       }),
       logo: false,
       target: id,
     });
-
-    // map.getLayers().forEach(layer => {
-    //   if (layer instanceof TileLayer) {
-    //     const source = layer.getSource();
-    //     if (source instanceof WMTS) {
-    //       source.setRenderReprojectionEdges(true);
-    //     }
-    //   }
-    // });
 
     // 사용자가 지도 선택할 수 있게 만든 버튼
     const divMapControlContainer = document.createElement('div');
